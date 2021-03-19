2fc95840b2d54865b0e2075a783d1c92ac6e3add
dlong <dlong@hawthornegc.com>
2021-03-19 13:21:25 -0700

    Add project files.

---
+++ Library/Models/Config.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library.Models
+{
+    public class Config
+    {
+        public string Token { get; set; }
+    }
+}
